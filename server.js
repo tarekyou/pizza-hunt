a1298d65508bae91d8e3bf944be1830fdff28d3e
tarekyou <89763835+tarekyou@users.noreply.github.com>
2022-01-09 23:52:17 -0800

    server

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(require("./routes"));
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost:27017/pizza-hunt",
   {
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
